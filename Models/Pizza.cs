811f0c593ea0bed935efe6fb3bea5f38638092ae
Giuseppe Lo Re <loregiuseppe0@gmail.com>
2022-12-01 13:37:34 +0100

    Aggiungere i file di progetto.

---
+++ Models/Pizza.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [StringLength(50, ErrorMessage = "Il titolo non può essere più lungo di 50 caratteri")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [StringLength(200, ErrorMessage = "La descrizione non può essere più lunga di 200 caratteri")]
+        [Column(TypeName = "text")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        public string Image { get; set; }
+        public float Price { get; set; }
+
+        public Pizza(string name, string description, string image, float price)
+        {
+            Name = name;
+            Description = description;
+            Image = image;
+            Price = price;
+        }
+
+        public Pizza()
+        {
+
+        }
+    }
+}
